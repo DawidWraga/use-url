3cb9b992d7634e019c02b9eea1da746775472caa
DawidWraga <dpwraga@gmail.com>
2023-05-03 20:38:24 +0100

    fix: test cases

--- jest.config.mjs
+++ jest.config.mjs
@@ -10,6 +10,22 @@ const config = {
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
 	testEnvironment: 'jest-environment-jsdom',
+
+	// // testEnvironment: 'node',
+	preset: 'ts-jest',
+	moduleNameMapper: {
+		'^~/(.*)$': '<rootDir>/src/$1',
+	},
+	// setupFilesAfterEnv: ['./jest.setup.ts'],
+	// moduleNameMapper: {
+	// 	'^~/(.*)$': '<rootDir>/src/$1',
+	// },
+	// transform: {
+	// 	'^.+\\.tsx?$': 'ts-jest',
+	// },
+
+	// testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	// moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
